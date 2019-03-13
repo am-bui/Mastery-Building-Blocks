2467c233e8236d2772dd794641213b5b5ef81257
Aimi <aimi@elaiam.com>
2019-03-13 11:56:44 -0500

    027-computeAreaOfATriangle.js

---
+++ 027-computeAreaOfATriangle.js
@@ -0,0 +1,29 @@
+/*
+Write a function called “computeAreaOfATriangle”.
+
+Given the base and height of a triangle, “computeAreaOfATriangle” returns its area.
+
+var output = computeAreaOfATriangle(4, 6);
+console.log(output); // --> 12
+*/
+
+
+
+function computeAreaOfATriangle(base, height) {
+  return (base * height) / 2;
+}
+
+
+
+/*
+Your Code Should Pass:
+
+describe("computeAreaOfATriangle", function() {
+  it("should return a number", function() {
+    expect(typeof computeAreaOfATriangle(6, 4)).toBe("number");
+  });
+  it("should return the area of a triangle", function() {
+    expect(computeAreaOfATriangle(6, 4)).toBe(12);
+  });
+});
+*/
