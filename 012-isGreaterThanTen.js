7392790d31db5ca1062c83b350ee625fd87f1d8a
Aimi <aimi@elaiam.com>
2019-03-12 23:27:12 -0500

    012-isGreaterThanTen.js

--- 012-isGreaterThanTen.js
+++ 012-isGreaterThanTen.js
@@ -10,3 +10,23 @@ console.log(output); // --> true
 function isGreaterThan10(num){
   return num > 10;
 }
+
+
+/*
+Your Code Should Pass:
+
+describe("isGreaterThan10", function() {
+  it("should return a boolean", function() {
+    expect(typeof isGreaterThan10(40)).toEqual("boolean");
+  });
+  it("should return false for a number less than 10", function() {
+    expect(isGreaterThan10(4)).toBe(false);
+  });
+  it("should return true for a number greater than 10", function() {
+    expect(isGreaterThan10(40)).toBe(true);
+  });
+  it("should return false for the number 10", function() {
+    expect(isGreaterThan10(10)).toBe(false);
+  });
+});
+*/
