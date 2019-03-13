d18183264cba701063da336b2fddc300e5cad13d
Aimi <aimi@elaiam.com>
2019-03-12 23:15:13 -0500

    010-getLengthOfWord.js

--- 010-getLengthOfWord.js
+++ 010-getLengthOfWord.js
@@ -9,3 +9,19 @@ Given a word, “getLengthOfWord” returns the length of the given word.*/
 function getLengthOfWord(word) {
   return word.length;
 }
+
+
+/*
+Your Code Should Pass:
+describe("getLengthOfWord", function() {
+  it("should return a number", function() {
+    expect(typeof getLengthOfWord("something")).toBe("number");
+  });
+  it("should return the length of the passed in word", function() {
+    expect(getLengthOfWord("random")).toBe(6)
+  });
+  it("should return the length of an empty word", function() {
+    expect(getLengthOfWord("")).toBe(0)
+  });
+});
+*/
