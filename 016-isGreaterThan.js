8670d3ce1f1f11566dc796a0090b0325e355a2a4
Aimi <aimi@elaiam.com>
2019-03-12 22:37:55 -0500

    016-isGreaterThan.js

---
+++ 016-isGreaterThan.js
@@ -0,0 +1,12 @@
+/*
+Write a function called “isGreaterThan”.
+Given 2 numbers, “isGreaterThan” returns whether num2 is greater than num1.
+
+var output = isGreaterThan(11, 10);
+console.log(output); // --> false
+*/
+
+
+function isGreaterThan(num1, num2) {
+  return (num2 > num1);
+}
