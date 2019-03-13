26474e5a2fcf51cce1c0f2dbbcd398f034d21abb
Aimi <aimi@elaiam.com>
2019-03-12 23:26:46 -0500

    011-getLengthOfTwoWords.js

--- 011-getLengthOfTwoWords.js
+++ 011-getLengthOfTwoWords.js
@@ -9,3 +9,17 @@ Given 2 words, “getLengthOfTwoWords” returns the sum of their lengths.*/
 function getLengthOfTwoWords(word1, word2){
   return word1.length + word2.length;
 }
+
+
+/*
+Your Code Should Pass:
+
+describe("getLengthOfTwoWords", function() {
+  it("should return a number", function() {
+    expect(typeof getLengthOfTwoWords("two", "words")).toBe("number");
+  });
+  it("should return the sum length of two words", function() {
+    expect(getLengthOfTwoWords("one", "two")).toBe(6)
+  });
+});
+*/
