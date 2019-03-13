5dfc772baf0ab44ba0d0baf77abc5e215d5b9a7e
Aimi <aimi@elaiam.com>
2019-03-13 11:49:11 -0500

    024-isEvenLength.js

---
+++ 024-isEvenLength.js
@@ -0,0 +1,32 @@
+/*
+Write a function called “isEvenLength”.
+
+Given a word, “isEvenLength” returns whether the length of the word is even.
+
+var output = isEvenLength('wow');
+console.log(output); // --> false
+*/
+
+
+
+function isEvenLength(word) {
+  return word.length % 2 === 0;
+}
+
+
+
+/*
+Your Code Should Pass:
+
+describe("isEvenLength", function() {
+  it("should return a boolean", function() {
+    expect(typeof isEvenLength("wow")).toEqual("boolean");
+  });
+  it("should return if the length of the word is even", function() {
+    expect(isEvenLength("specials")).toBe(true);
+  });
+  it("should return true if the string is empty", function() {
+    expect(isEvenLength("")).toBe(true);
+  });
+});
+*/
