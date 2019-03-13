49f97df667e74de2c0f83ba844841448a4127b4a
Aimi <aimi@elaiam.com>
2019-03-12 22:55:38 -0500

    005-getProperty.js

--- 005-getProperty.js
+++ 005-getProperty.js
@@ -15,3 +15,25 @@ Notes:
 function getProperty(obj, key) {
   return obj[key];
 }
+
+
+
+/*
+Your Code Should Pass:
+describe("getProperty", function() {
+  var obj;
+
+  beforeEach(function() {
+    obj = {
+      name: "You"
+    }
+  });
+
+  it("should return the value of the property located in the object at the passed in key", function() {
+    expect(getProperty(obj, "name")).toBe("You");
+  });
+  it("should return undefined when there is no property at the passed in key", function() {
+    expect(getProperty(obj, "age")).toBe(undefined);
+  });
+});
+*/
