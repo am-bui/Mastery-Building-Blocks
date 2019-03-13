ffe904532883c47a75a7b8454c90c714abbc9527
Aimi <aimi@elaiam.com>
2019-03-12 23:27:31 -0500

    013-isLessThan30.js

--- 013-isLessThan30.js
+++ 013-isLessThan30.js
@@ -9,3 +9,23 @@ console.log(output); // --> true*/
 function isLessThan30(num){
   return num < 30;
 }
+
+
+/*
+Your Code Should Pass:
+
+describe("isLessThan30", function() {
+  it("should return a boolean", function() {
+    expect(typeof isLessThan30(40)).toEqual("boolean");
+  });
+  it("should return true for a number less than 30", function() {
+    expect(isLessThan30(4)).toBe(true);
+  });
+  it("should return false for a number greater than 30", function() {
+    expect(isLessThan30(400)).toBe(false);
+  });
+  it("should return false for the number 30", function() {
+    expect(isLessThan30(30)).toBe(false);
+  });
+});
+*/
