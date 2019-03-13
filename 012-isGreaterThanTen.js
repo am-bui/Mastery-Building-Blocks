8f214467060b1ca7c144dc51ea75b04553b14d64
Aimi <aimi@elaiam.com>
2019-03-12 21:46:36 -0500

    012-isGreaterThanTen.js

---
+++ 012-isGreaterThanTen.js
@@ -0,0 +1,12 @@
+/*
+Write a function called “isGreaterThan10”.
+Given a number, “isGreaterThan10” returns whether the given number is greater than 10.
+
+var output = isGreaterThan10(11);
+console.log(output); // --> true
+*/
+
+
+function isGreaterThan10(num){
+  return num > 10;
+}
