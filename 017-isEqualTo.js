6dd2d3fcec9f63d25094b27bfae70d2a607bd50e
Aimi <aimi@elaiam.com>
2019-03-12 22:40:38 -0500

    017-isEqualTo.js

---
+++ 017-isEqualTo.js
@@ -0,0 +1,12 @@
+/*
+Write a function called “isEqualTo”.
+Given 2 numbers, “isEqualTo” returns whether num2 is equal to num1.
+
+var output = isEqualTo(11, 10);
+console.log(output); // --> false
+*/
+
+
+function isEqualTo(num1, num2) {
+  return (num2 == num1);
+}
