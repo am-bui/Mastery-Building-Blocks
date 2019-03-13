da4d73c9a5edcfa56af9ca81b093f56448477ca3
Aimi <aimi@elaiam.com>
2019-03-12 22:53:42 -0500

    003-isOldEnoughToVote.js

--- 003-isOldEnoughToVote.js
+++ 003-isOldEnoughToVote.js
@@ -11,3 +11,19 @@ Notes:
 function isOldEnoughToVote(age){
   return age >= 18;
 }
+
+
+/*
+Your Code Should Pass:
+describe("isOldEnoughToVote", function() {
+  it("should return a boolean", function() {
+    expect(typeof isOldEnoughToVote(40)).toEqual("boolean");
+  });
+  it("should return whether the age is greater than 18", function() {
+    expect(isOldEnoughToVote(40)).toBe(true);
+  });
+  it("should return true if the age is 18", function() {
+    expect(isOldEnoughToVote(18)).toBe(true);
+  });
+});
+*/
