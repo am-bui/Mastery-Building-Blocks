34918e7d89a678ec3246e6e57b4f8dbf19c265eb
Aimi <aimi@elaiam.com>
2019-03-12 22:42:02 -0500

    018-isEven.js

---
+++ 018-isEven.js
@@ -0,0 +1,12 @@
+/*
+Write a function called “isEven”.
+Given a number, “isEven” returns whether it is even.
+
+var output = isEven(11);
+console.log(output); // --> false
+*/
+
+
+function isEven(num){
+  return num % 2 === 0;
+}
