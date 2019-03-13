d8484d82217c3a6800c107d15a46471a8dcd25f0
Aimi <aimi@elaiam.com>
2019-03-12 23:27:46 -0500

    014-equalsTen.js

--- 014-equalsTen.js
+++ 014-equalsTen.js
@@ -9,3 +9,23 @@ console.log(output); // --> false*/
 function equalsTen(num){
   return num === 10;
 }
+
+
+/*
+Your Code Should Pass:
+
+describe("equalsTen", function() {
+  it("should return a boolean", function() {
+    expect(typeof equalsTen(10)).toEqual("boolean");
+  });
+  it("should return true for the number 10", function() {
+    expect(equalsTen(10)).toBe(true);
+  });
+  it("should return false for a number greater than 10", function() {
+    expect(equalsTen(400)).toBe(false);
+  });
+  it("should return false for the number 10", function() {
+    expect(equalsTen(3)).toBe(false);
+  });
+});
+*/
