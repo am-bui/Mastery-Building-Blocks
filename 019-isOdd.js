db500d060a0a9dbe14abf092dbbad3327c576028
Aimi <aimi@elaiam.com>
2019-03-12 22:43:17 -0500

    019-isOdd.js

---
+++ 019-isOdd.js
@@ -0,0 +1,12 @@
+/*
+Write a function called “isOdd”.
+Given a number, “isOdd” returns whether the given number is odd.
+
+var output = isOdd(9);
+console.log(output); // --> true
+*/
+
+
+function isOdd(num){
+  return num % 2 !== 0;
+}
