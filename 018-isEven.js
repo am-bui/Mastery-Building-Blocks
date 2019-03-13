582d816d53d0f67768d2fea632490b275a42cb30
Aimi <aimi@elaiam.com>
2019-03-12 23:36:44 -0500

    018-isEven.js

--- 018-isEven.js
+++ 018-isEven.js
@@ -10,3 +10,20 @@ console.log(output); // --> false
 function isEven(num){
   return num % 2 === 0;
 }
+
+
+/*
+Your Code Should Pass:
+
+describe("isEven", function() {
+  it("should return a boolean", function() {
+    expect(typeof isEven(40)).toEqual("boolean");
+  });
+  it("should return if the number is even", function() {
+    expect(isEven(8)).toBe(true);
+  });
+  it("should return true if the number is 0", function() {
+    expect(isEven(0)).toBe(true);
+  });
+});
+*/
