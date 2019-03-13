e41debea74a1eb124c499d8cf8e305f7fe56d288
Aimi <aimi@elaiam.com>
2019-03-12 21:55:41 -0500

    013-isLessThan30.js

---
+++ 013-isLessThan30.js
@@ -0,0 +1,11 @@
+/*
+Write a function called “isLessThan30”.
+Given a number, “isLessThan30” returns whether the given number is less than 30.
+
+var output = isLessThan30(9);
+console.log(output); // --> true*/
+
+
+function isLessThan30(num){
+  return num < 30;
+}
