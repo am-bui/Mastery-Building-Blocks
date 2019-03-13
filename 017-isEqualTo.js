0b229e45c67174da5796da6f931ea6f5dd91440c
Aimi <aimi@elaiam.com>
2019-03-12 23:35:45 -0500

    017-isEqualTo.js

--- 017-isEqualTo.js
+++ 017-isEqualTo.js
@@ -10,3 +10,23 @@ console.log(output); // --> false
 function isEqualTo(num1, num2) {
   return (num2 == num1);
 }
+
+
+/*
+Your Code Should Pass:
+
+describe("isEqualTo", function() {
+  it("should return a boolean", function() {
+    expect(typeof isEqualTo(40, 30)).toEqual("boolean");
+  });
+  it("should return false if num2 is greater than num1", function() {
+    expect(isEqualTo(20, 200)).toBe(false);
+  });
+  it("should return false if num2 is less than num1", function() {
+    expect(isEqualTo(20, 2)).toBe(false);
+  });
+  it("should return true if the numbers are equal", function() {
+    expect(isEqualTo(20, 20)).toBe(true);
+  });
+});
+*/
