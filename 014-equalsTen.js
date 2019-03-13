7f469bd94925e1575748e83776014fb5d6c8c75f
Aimi <aimi@elaiam.com>
2019-03-12 22:21:26 -0500

    014-equalsTen.js

---
+++ 014-equalsTen.js
@@ -0,0 +1,11 @@
+/*
+Write a function called “equalsTen”.
+Given a number, “equalsTen” returns whether or not the given number is 10.
+
+var output = equalsTen(9);
+console.log(output); // --> false*/
+
+
+function equalsTen(num){
+  return num === 10;
+}
