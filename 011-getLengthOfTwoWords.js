5870299fdbc772f2d2fe68c2f6c56b25fccc3779
Aimi <aimi@elaiam.com>
2019-03-12 20:07:27 -0500

    added file 011

---
+++ 011-getLengthOfTwoWords.js
@@ -0,0 +1,11 @@
+/*
+Write a function called “getLengthOfTwoWords”.
+Given 2 words, “getLengthOfTwoWords” returns the sum of their lengths.*/
+
+// var output = getLengthOfTwoWords('some', 'words');
+// console.log(output); // --> 9
+
+
+function getLengthOfTwoWords(word1, word2){
+  return word1.length + word2.length;
+}
