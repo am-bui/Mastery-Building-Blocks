2cdddcee4cd3ec35b0f458f6fac880873946e301
Aimi <aimi@elaiam.com>
2019-03-12 23:10:54 -0500

    006-addProperty.js

--- 006-addProperty.js
+++ 006-addProperty.js
@@ -9,3 +9,24 @@ Given an object, and a key, “addProperty” sets a new property on the given o
 function addProperty(obj, key) {
   obj[key] = true;
 }
+
+
+/*
+Your Code Should Pass:
+describe("getProperty", function() {
+  var obj;
+
+  beforeEach(function() {
+    obj = {
+      name: "You"
+    }
+  });
+
+  it("should return the value of the property located in the object at the passed in key", function() {
+    expect(getProperty(obj, "name")).toBe("You");
+  });
+  it("should return undefined when there is no property at the passed in key", function() {
+    expect(getProperty(obj, "age")).toBe(undefined);
+  });
+});
+*/
