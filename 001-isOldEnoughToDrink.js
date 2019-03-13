21edca9e42aebb9bcfc42c1f296d76ebc5ff87c6
Aimi <aimi@elaiam.com>
2019-03-12 22:50:03 -0500

    001-isOldEnoughToDrink.js

--- 001-isOldEnoughToDrink.js
+++ 001-isOldEnoughToDrink.js
@@ -2,11 +2,33 @@
 Write a function called “isOldEnoughToDrink”.
 Given a number, in this case an age, “isOldEnoughToDrink” returns whether a person of this given age is old enough to legally drink in the United States.
 Notes:
-* The legal drinking age in the United States is 21. */
+* The legal drinking age in the United States is 21.
 
-// var output = isOldEnoughToDrink(22);
-// console.log(output); // --> true
+var output = isOldEnoughToDrink(22);
+console.log(output); // --> true
+*/
+
+
+
 
 function isOldEnoughToDrink(age) {
  return age >= 21;
 }
+
+
+
+
+/*
+Your Code Should Pass:
+describe("isOldEnoughToDrink", function() {
+  it("should return a boolean", function() {
+    expect(typeof isOldEnoughToDrink(40)).toEqual("boolean");
+  });
+  it("should return whether the age is greater than 21", function() {
+    expect(isOldEnoughToDrink(40)).toBe(true);
+  });
+  it("should return true if the age is 21", function() {
+    expect(isOldEnoughToDrink(21)).toBe(true);
+  });
+});
+*/
