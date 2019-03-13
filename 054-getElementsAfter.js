700fe32629bcd6fa9151354f311076aeb9ca229d
Aimi <aimi@elaiam.com>
2019-03-13 13:05:38 -0500

    054-getElementsAfter.js

--- 054-getElementsAfter.js
+++ 054-getElementsAfter.js
@@ -1,7 +1,7 @@
 // Write a function called “getElementsAfter”.
-//
+
 // Given an array and an index, “getElementsAfter” returns a new array with all the elements after (but not including) the given index.
-//
+
 // var output = getElementsAfter(['a', 'b', 'c', 'd', 'e'], 2);
 // console.log(output); // --> ['d', 'e']
 
