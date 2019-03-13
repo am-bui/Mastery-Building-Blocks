2eac56226b8093f0d42e84a5d7b0e3ee82214b4b
Aimi <aimi@elaiam.com>
2019-03-12 22:31:09 -0500

    015-isLessThan.js

---
+++ 015-isLessThan.js
@@ -0,0 +1,12 @@
+/*
+Write a function called “isLessThan”.
+Given 2 numbers, “isLessThan” returns whether num2 is less than num1.
+
+var output = isLessThan(9, 4);
+console.log(output); // --> true
+*/
+
+
+function isLessThan(num1, num2) {
+  return num2 < num1;
+}
