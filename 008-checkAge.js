7198c3d8d2d513ee46902ebfad732d77eee0db33
Aimi <aimi@elaiam.com>
2019-03-12 23:14:36 -0500

    008-checkAge.js

--- 008-checkAge.js
+++ 008-checkAge.js
@@ -13,3 +13,23 @@ Naturally, replace “{insert_name_here}” with the given name. :) */
 function checkAge(name, age){
   return age >= 21 ? `Welcome, ${name}!` : `Go home, ${name}!`;
 }
+
+
+
+/*
+Your Code Should Pass:
+describe("checkAge", function() {
+  it("should return a string", function() {
+    expect(typeof(checkAge("Dan", "24"))).toBe("string");
+  });
+  it("should welcome someone over 21", function() {
+    expect(checkAge("Dan", "24")).toBe("Welcome, Dan!");
+  });
+  it("should welcome a 21 year old", function() {
+    expect(checkAge("Toni", "21")).toBe("Welcome, Toni!");
+  });
+  it("should bounce someone under 21", function() {
+    expect(checkAge("Rad", "4")).toBe("Go home, Rad!");
+  });
+});
+*/
