169cad222fae3bc37959687a038c1afafd130cbf
Aimi <aimi@elaiam.com>
2019-03-12 23:37:55 -0500

    019-isOdd.js

--- 019-isOdd.js
+++ 019-isOdd.js
@@ -10,3 +10,20 @@ console.log(output); // --> true
 function isOdd(num){
   return num % 2 !== 0;
 }
+
+
+/*
+Your Code Should Pass:
+
+describe("isOdd", function() {
+  it("should return a boolean", function() {
+    expect(typeof isOdd(40)).toEqual("boolean");
+  });
+  it("should return if the number is odd", function() {
+    expect(isOdd(7)).toBe(true);
+  });
+  it("should return false if the number is 0", function() {
+    expect(isOdd(0)).toBe(false);
+  });
+});
+*/
