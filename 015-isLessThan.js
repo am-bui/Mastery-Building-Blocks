250c93827bf5a14600a15d23fc8d4d67deef5abe
Aimi <aimi@elaiam.com>
2019-03-12 23:33:38 -0500

    015-isLessThan.js

--- 015-isLessThan.js
+++ 015-isLessThan.js
@@ -10,3 +10,20 @@ console.log(output); // --> true
 function isLessThan(num1, num2) {
   return num2 < num1;
 }
+
+
+/*
+Your Code Should Pass:
+
+describe("isLessThan", function() {
+  it("should return a boolean", function() {
+    expect(typeof isLessThan(40, 30)).toEqual("boolean");
+  });
+  it("should return whether num2 is less than num1", function() {
+    expect(isLessThan(20, 200)).toBe(false);
+  });
+  it("should return false if the numbers are equal", function() {
+    expect(isLessThan(20, 20)).toBe(false);
+  });
+});
+*/
